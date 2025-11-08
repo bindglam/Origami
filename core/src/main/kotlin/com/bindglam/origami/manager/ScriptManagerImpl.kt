7a5babcbe40778a56d313987211bd5d355ccdecb
Woobeen Jeon <woobeen0516@gmail.com>
2025-11-08 20:33:28 +0900

    리팩토링

--- core/src/main/kotlin/com/bindglam/origami/manager/ScriptManagerImpl.kt
+++ core/src/main/kotlin/com/bindglam/origami/manager/ScriptManagerImpl.kt
@@ -1,50 +1,15 @@
 package com.bindglam.origami.manager
 
-import com.bindglam.origami.api.OrigamiProvider
 import com.bindglam.origami.api.manager.ScriptManager
 import com.bindglam.origami.api.script.Script
-import com.bindglam.origami.api.script.exceptions.IllegalArgumentsException
-import com.bindglam.origami.api.script.exceptions.RuntimeException
 import com.bindglam.origami.api.script.exceptions.ScriptException
 import com.bindglam.origami.api.script.interpreter.SymbolTable
-import com.bindglam.origami.api.script.interpreter.value.primitive.function.BuiltInFunction
-import com.bindglam.origami.api.script.interpreter.value.bukkit.Entity
-import com.bindglam.origami.api.script.interpreter.value.primitive.function.Function
-import com.bindglam.origami.api.script.interpreter.value.math.Location
-import com.bindglam.origami.api.script.interpreter.value.math.Vector3
-import com.bindglam.origami.api.script.interpreter.value.primitive.List
-import com.bindglam.origami.api.utils.math.LocationAdaptable
 import com.bindglam.origami.api.script.interpreter.value.primitive.Number
-import com.bindglam.origami.api.script.interpreter.value.primitive.function.Argument
-import com.bindglam.origami.script.function.CosFunction
-import com.bindglam.origami.script.function.DamageFunction
-import com.bindglam.origami.script.function.DelayFunction
-import com.bindglam.origami.script.function.EntityToLocationFunction
-import com.bindglam.origami.script.function.GetNearbyEntitiesFunction
-import com.bindglam.origami.script.function.KnockbackFunction
-import com.bindglam.origami.script.function.LengthFunction
-import com.bindglam.origami.script.function.LocationFunction
-import com.bindglam.origami.script.function.LocationToVector3Function
-import com.bindglam.origami.script.function.NormalizeFunction
-import com.bindglam.origami.script.function.ParticleFunction
-import com.bindglam.origami.script.function.PlaySoundFunction
-import com.bindglam.origami.script.function.PrintFunction
-import com.bindglam.origami.script.function.RegisterListenerFunction
-import com.bindglam.origami.script.function.SendMessageFunction
-import com.bindglam.origami.script.function.SinFunction
-import com.bindglam.origami.script.function.ToRadiansFunction
-import com.bindglam.origami.script.function.Vector3Function
-import net.kyori.adventure.text.minimessage.MiniMessage
-import org.bukkit.Bukkit
-import org.bukkit.Particle
-import org.bukkit.entity.LivingEntity
-import org.bukkit.util.Vector
-import org.joml.Vector3d
+import com.bindglam.origami.api.script.interpreter.value.primitive.function.BuiltInFunction
+import com.bindglam.origami.script.function.*
 import java.io.File
 import java.util.*
 import java.util.logging.Logger
-import kotlin.math.cos
-import kotlin.math.sin
 
 object ScriptManagerImpl : ScriptManager {
     private val scriptsFolder = File("plugins/Origami/scripts")
